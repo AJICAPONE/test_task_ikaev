666df79768d07616dea95abdd77f004c35b3cf32
AJICAPONE <icaev888@gmail.com>
2021-01-09 19:09:46 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import html from "./template/layout/index.pug"
+// Import styles
+import style from "./styles/main.scss";
+import bs from "../node_modules/bootstrap/scss/bootstrap.scss";
+// Import scripts
+import script from "./scripts/main.js";
+import bootstrap from "../node_modules/bootstrap/dist/js/bootstrap.js";
+console.log('all files loaded');
